e82218f039704f6ebe487a9229b78aa26e69d194
lee <unknown9732@naver.com>
2022-05-14 16:36:50 +0900

    getDateString 파일 분리

---
+++ common/functions.js
@@ -0,0 +1,23 @@
+function getDateString(timestamp) {
+  const date = new Date(timestamp);
+  let year = date.getFullYear().toString();
+
+  let month = date.getMonth() + 1;
+  month = month < 10 ? '0' + month.toString() : month.toString();
+
+  let day = date.getDate();
+  day = day < 10 ? '0' + day.toString() : day.toString();
+
+  let hour = date.getHours();
+  hour = hour < 10 ? '0' + hour.toString() : hour.toString();
+
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? '0' + minutes.toString() : minutes.toString();
+
+  let seconds = date.getSeconds();
+  seconds = seconds < 10 ? '0' + seconds.toString() : seconds.toString();
+
+  return String().concat(year, month, day, hour, minutes, seconds);
+}
+
+module.exports = getDateString;
